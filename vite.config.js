ae2d03638a398fd3a5821dca11c27bf2c2fdca3b
umwariyvette95-jpg <umwariyvette95@gmail.com>
2026-06-20 23:19:18 +0200

    editing vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// Basic Vite config — just tells Vite we're using React
 export default defineConfig({
   plugins: [react()],
-});
+})
